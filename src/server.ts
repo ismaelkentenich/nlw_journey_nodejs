05e89e07f635283731a2f40d82bc9276ae1751e1
Ismael Andrade <compasso_ismael.andrade@comprealugueagora.com.br>
2024-07-12 14:54:53 -0300

    feat: create route for creating and listing links

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,36 @@
-import fastify from "fastify"
-import cors from "@fastify/cors"
-import { createTrip } from "./routes/create-trip"
-import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import fastify from "fastify";
+import cors from "@fastify/cors";
+import { createTrip } from "./routes/create-trip";
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
 import { confirmTrip } from "./routes/confirm-trip";
 import { confirmParticipant } from "./routes/confirm-participant";
 import { createActivity } from "./routes/create-activity";
 import { getActivities } from "./routes/get-activities";
+import { createLink } from "./routes/create-links";
+import { getLinks } from "./routes/get-links";
 
-const app = fastify()
+const app = fastify();
 
 app.register(cors, {
-    origin: '*'
-} )
+  origin: "*",
+});
 
 // Add schema validator and serializer
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-app.register(createTrip)
-app.register(confirmTrip)
-app.register(confirmParticipant)
-app.register(createActivity)
-app.register(getActivities)
+app.register(createTrip);
+app.register(confirmTrip);
+app.register(confirmParticipant);
+app.register(createActivity);
+app.register(getActivities);
+app.register(createLink);
+app.register(getLinks);
 
 
-
-
-app.listen({port: 3333}).then(() => {
-    console.log("Server runnig!");
-})
+app.listen({ port: 3333 }).then(() => {
+  console.log("Server runnig!");
+});
