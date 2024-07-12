3b889520dd2540ae4db8fb36b59545f404ec93c0
Ismael Andrade <compasso_ismael.andrade@comprealugueagora.com.br>
2024-07-12 17:02:22 -0300

    feat: improve error handling mechanism

--- src/routes/get-participant.ts
+++ src/routes/get-participant.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
+import { ClientError } from "../errors/client-error";
 
 export async function getParticipant(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().get(
@@ -27,7 +28,7 @@ export async function getParticipant(app: FastifyInstance) {
       });
 
       if (!participantId) {
-        throw new Error("Paricipant not found.");
+        throw new ClientError("Paricipant not found.");
       }
 
       return { participant };
